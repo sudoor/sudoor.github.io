eea0c4dae636b464755f9cbbd0c63effb53ae064
王佳 <wangjia_1919@163.com>
2019-02-27 12:21:44 +0800

    init with vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'NineSwordsMonster',
+  description: 'NineSwordsMonster',
+  base: '/',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  ga: "UA-118953951-2",
+  locales: {
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+    },
+  },
+  theme: "vuepress-theme-nine",
+  themeConfig: {},
+  plugins: [
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }]
+  ],
+  evergreen: true
+}
