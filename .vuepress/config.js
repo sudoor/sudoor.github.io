9a38b4c1af8cbf3cde888492deafc3217ab741a3
王佳 <wangjia_1919@163.com>
2019-04-18 20:33:19 +0800

    fix config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,26 +1,90 @@
 module.exports = {
-  title: 'NineSwordsMonster',
-  description: 'NineSwordsMonster',
-  base: '/',
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
-  ],
-  ga: "UA-118953951-2",
+  // 网站 Title
+  title: 'Nine',
+
+  // 网站描述
+  description: 'Nine Blog',
+
+  // 网站语言
   locales: {
     '/': {
-      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+      lang: 'zh-CN',
     },
   },
-  theme: "theme/vuepress-theme-nine",
+
+  // 使用的主题
+  theme: 'nine',
+
+  // 主题配置
   themeConfig: {
-    sidebar: 'auto'
+    // 主题语言，参考下方 [主题语言] 章节
+    // lang: require('vuepress-theme-nine/lib/langs/zh-CN'),
+
+    // 个人信息（没有或不想设置的，删掉对应字段即可）
+    personalInfo: {
+      // 昵称
+      nickname: 'nine',
+
+      // 个人简介
+      description: 'Coder',
+
+      // 电子邮箱
+      email: 'wangjia_1919@163.com',
+
+      // 所在地
+      location: 'Suz, China',
+
+      // 组织
+      organization: '',
+
+      // 头像
+      // 设置为外部链接
+      avatar: '/img/avatar.jpg',
+      // 或者放置在 .vuepress/public 文件夹，例如 .vuepress/public/img/avatar.jpg
+      // avatar: '/img/avatar.jpg',
+
+
+      // 社交平台帐号信息
+      sns: {
+        // Github 帐号和链接
+        github: {
+          account: 'nine',
+          link: 'https://github.com/NineSwordsMonster',
+        }
+      },
+    },
+
+    // 上方 header 的相关设置
+    header: {
+      // header 的背景，可以使用图片，或者随机变化的图案（geopattern）
+      background: {
+        // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
+        url: '/assets/img/bg.jpg',
+
+        // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
+        useGeo: true,
+      },
+
+      // 是否在 header 显示标题
+      showTitle: true,
+    },
+
+    // 是否显示文章的最近更新时间
+    lastUpdated: true,
+
+    // 顶部导航栏内容
+    nav: [
+      { text: '首页', link: '/', exact: true },
+      { text: '文章', link: '/posts/', exact: false },
+    ],
+
+    // 评论配置，参考下方 [页面评论] 章节
+    comments: {
+      owner: 'NineSwordsMonster',
+      repo: 'gitment-comments',
+      clientId: '6df5e3f1c9df36da16f8',
+      clientSecret: 'b1e8d8bf0f60f1cffd7aedfed12e2383553028ea',
+      autoCreateIssue: process.env.NODE_ENV !== 'development',
+    },
   },
-  plugins: [
-    // ['@vuepress/search', {
-    //   searchMaxSuggestions: 10
-    // }]
-  ],
-  evergreen: true,
-  less: {
-  }
 }
