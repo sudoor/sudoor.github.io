8448f390d0ca5a63cf0014c1a7534c9b9547d5be
抱紧我的小鲤鱼 <wangjia_1919@163.com>
2019-07-09 02:14:32 +0800

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,31 +1,31 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
-set -e
+# # 确保脚本抛出遇到的错误
+# set -e
 
-# 生成静态文件
-npm run build
+# # 生成静态文件
+# npm run build
 
-# 进入生成的文件夹
-cd dist
+# # 进入生成的文件夹
+# cd dist
 
-ls -l
-echo "waiting ....."
+# ls -l
+# echo "waiting ....."
 
-sleep 4
-# 如果是发布到自定义域名
-echo 'w.wzzx.xyz' > CNAME
+# sleep 4
+# # 如果是发布到自定义域名
+# echo 'w.wzzx.xyz' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy @ `date`'
+# git init
+# git add -A
+# git commit -m 'deploy @ `date`'
 
-# 如果发布到 https://<USER_NAME>.github.io
-#git push -f git@github.com:${USER_NAME}/{USER_NAME}.github.io.git master
+# # 如果发布到 https://<USER_NAME>.github.io
+# #git push -f git@github.com:${USER_NAME}/{USER_NAME}.github.io.git master
 
-# 如果发布到 https://<USER_NAME>.github.io/<REPO>
-# git push -f git@github.com:<USER_NAME>/<REPO>.git master:gh-pages
+# # 如果发布到 https://<USER_NAME>.github.io/<REPO>
+# # git push -f git@github.com:<USER_NAME>/<REPO>.git master:gh-pages
 
-git push -f https://${ACCESS_TOKEN}@github.com:${USER_NAME$}/${USER_NAME}.github.io.git master
+# git push -f https://${ACCESS_TOKEN}@github.com:${USER_NAME$}/${USER_NAME}.github.io.git master
 
-cd -
+# cd -
