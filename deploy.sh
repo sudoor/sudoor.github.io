3778a37fb27265e51d30f36dce5638eef4dd9127
NineSwordsMonster <wangjia_1919@163.com>
2019-12-13 10:05:20 +0800

    init gh-page

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
 npm run build
-
-# 进入生成的文件夹
 cd dist
-
-# 如果是发布到自定义域名
 echo 'hub.alili.fun' > CNAME
-
 git init
 git add .
 git commit -am 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
+git checkout -b gh-page
 git remote add origin https://github.com/NineSwordsMonster/NineSwordsMonster.github.io.git
-git push -f -u --set-upstream origin gh-page
+git branch --set-upstream-to=origin/gh-page gh-page
+git push -f
 
 cd -
